7540b4a50378205e2a9bc7f1c7c12b5cd135837d
Brian David <xbriandavid@gmail.com>
2020-07-22 16:49:31 -0700

    updated

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -125,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
